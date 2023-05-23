12c202b12b7700e4b8e5a278ab32329035967940
vathin <134401419+vathin@users.noreply.github.com>
2023-05-23 17:34:53 +0300

    Add files via upload
    v 0.2 - Добавлен класс GameSprite(наследник класса sprite.Sprite),
    Добавлен класс Player(наследник класса GameSprite) и метод player_movement, отвечающий за движение игрока

--- ping_pong.py
+++ ping_pong.py
@@ -8,11 +8,23 @@ display.set_caption('ping pong')
 back_color = (70, 200, 250)
 background = Surface((1000, 600))
 background.fill(back_color)
+class GameSprite(sprite.Sprite):
+    def __init__(self, load_image, xpos, ypos, width, height, speed):
+        super().__init__()
+        self.image = transform.scale(image.load(load_image), (width, height))
+        self.rect = self.image.get_rect()
+        self.rect.x = xpos
+        self.rect.y = ypos
+        self.speed = speed
+    def update(self):
+        win.blit(self.image,(self.rect.x, self.rect.y))
 
-
-
-
-
+class Player(GameSprite):
+    def player_movement(self, Ydirection):
+        if self.Ydirection == 'up':
+            self.rect.y += self.speed
+        if self.Ydirection == 'down':
+            self.rect.y -= self.speed
 
 
 
